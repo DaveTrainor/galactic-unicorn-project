7e38f978b7bd18aba6e3c56bfb60b9ff851707b4
BushraAbdullahi <bushra1175@gmail.com>
2023-04-13 09:58:21 +0100

    move paddle up and down

--- main.py
+++ main.py
@@ -1,37 +1,68 @@
-from app.screen import load_screen
+from picounicorn import PicoUnicorn
+from picographics import PicoGraphics, DISPLAY_UNICORN_PACK, PEN_RGB888
 import time
-import network
-import settings
-from urequests import get as http_get
 
-screen = settings.screen
+display = PicoGraphics(display=DISPLAY_UNICORN_PACK, pen_type=PEN_RGB888)
+screen = PicoUnicorn()
+# screen = hardware
+# display = abstract screen
+x = 1
+y = 0
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(
-    settings.wifi_access_point,
-    settings.wifi_password
-)
+red = display.create_pen(255, 0, 0)
+blank = display.create_pen(0, 0, 0)
 
-while not wlan.isconnected():
-    print('Waiting for connection...')
-    time.sleep(1)
+while True:
+    if screen.is_pressed(screen.BUTTON_A):
+        y = max(0, y - 1)
+    elif screen.is_pressed(screen.BUTTON_B):
+        y = min(screen.get_height() - 2, y + 1)
+    
+    display.set_pen(blank)
+    display.clear()
+    display.set_pen(red)
+    display.pixel(x, y)
+    display.pixel(x, y+1)
+    screen.update(display)
+    time.sleep(0.1)
 
-ip_address, subnet, gateway, dns = wlan.ifconfig()
-print(f'ip: {ip_address}\ngateway: {gateway}')
+# from picounicorn import PicoUnicorn
+# from picographics import PicoGraphics, DISPLAY_UNICORN_PACK, PEN_RGB888
+# import time
 
-r = http_get("https://api.open-meteo.com/v1/forecast?latitude=53.55&longitude=2.01&current_weather=true&hourly"
-             "=temperature_2m,relativehumidity_2m,windspeed_10m")
+# # create a PicoGraphics framebuffer to draw into
+# graphics = PicoGraphics(display=DISPLAY_UNICORN_PACK, pen_type=PEN_RGB888)
 
-temp = str(r.json()['current_weather']['temperature'])
+# # create our GalacticUnicorn object
+# gu = PicoUnicorn()
 
-# temp = str(10.0)
-temp_display = temp + '°C'
-colour = 'GREEN'
-if float(temp) >= 20:
-    colour = 'RED'
-if float(temp) <= 4:
-    colour = 'BLUE'
+# # start position for scrolling (off the side of the display)
+# scroll = float(-PicoUnicorn.WIDTH)
 
-screen = load_screen(screen.get('driver'))
-screen().show_text(temp_display, 'centre', colour)
+# # message to scroll
+# MESSAGE = "Boo"
+
+# # pen colours to draw with
+# BLACK = graphics.create_pen(0, 0, 0)
+# PURPLE = graphics.create_pen(255, 0, 255)
+
+# while True:
+#     # determine the scroll position of the text
+#     width = graphics.measure_text(MESSAGE, 0.4)
+#     scroll += 0.25
+#     if scroll > width:
+#       scroll = float(-PicoUnicorn.WIDTH)
+
+#     # clear the graphics object
+#     graphics.set_pen(BLACK)
+#     graphics.clear()
+
+#     # draw the text
+#     graphics.set_pen(PURPLE)
+#     # text, x, y, wordwrap, scale, angle, spacing
+#     graphics.text(MESSAGE, round(0 - scroll), 0, scale=0.4)
+
+#     # update the display
+#     gu.update(graphics)
+
+#     time.sleep(0.02)
