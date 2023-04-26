7c2e2b5dc870b1c191eeb2cb795295969999b0d7
BushraAbdullahi <bushra1175@gmail.com>
2023-04-26 12:41:19 +0100

    refactored to display different paddle sizes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ devices = setup_devices()
 settings = app.settings.Settings()
 
 x_velocity = 1.0
-paddle_size = 3
+
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
@@ -16,8 +16,12 @@ BLUE = (0, 0, 255)
 
 class Paddle:
     def __init__(self, x_start, colour):
+        if settings.screen.driver == 'pico_unicorn':
+            self.size = 2
+        else:
+            self.size = 3
         self.x = int(x_start)
-        self.y = math.floor((devices.screen.attributes.height - paddle_size) / 2)
+        self.y = math.floor((devices.screen.attributes.height - self.size) / 2)
         self.colour = colour
 
 
@@ -37,12 +41,12 @@ def button_watcher():
     if devices.screen.is_pressed('left_1'):
         left_paddle.y = max(0, left_paddle.y - 1)
     elif devices.screen.is_pressed('left_2'):
-        left_paddle.y = min(devices.screen.attributes.height - paddle_size, left_paddle.y + 1)
+        left_paddle.y = min(devices.screen.attributes.height - left_paddle.size, left_paddle.y + 1)
 
     if devices.screen.is_pressed('right_1'):
         right_paddle.y = max(0, right_paddle.y - 1)
     elif devices.screen.is_pressed('right_2'):
-        right_paddle.y = min(devices.screen.attributes.height - paddle_size, right_paddle.y + 1)
+        right_paddle.y = min(devices.screen.attributes.height - right_paddle.size, right_paddle.y + 1)
 
 
 # class Game(self)
@@ -52,16 +56,16 @@ while True:
     button_watcher()
 
     devices.screen.clear()
-    devices.screen.rectangle(((left_paddle.x, left_paddle.y), (1, paddle_size)), left_paddle.colour)
+    devices.screen.rectangle(((left_paddle.x, left_paddle.y), (1, left_paddle.size)), left_paddle.colour)
 
-    devices.screen.rectangle(((right_paddle.x, right_paddle.y), (1, paddle_size)), right_paddle.colour)
+    devices.screen.rectangle(((right_paddle.x, right_paddle.y), (1, right_paddle.size)), right_paddle.colour)
     devices.screen.clear(((int(ball.x), ball.y), (1, 1)), ball.colour)
     ball.x += x_velocity
 
     if ball.x == devices.screen.attributes.width - 3 and\
-            (right_paddle.y <= ball.y <= right_paddle.y + paddle_size-1):
+            (right_paddle.y <= ball.y <= right_paddle.y + right_paddle.size-1):
         x_velocity = -x_velocity
-    elif ball.x == 2 and (left_paddle.x <= ball.y <= left_paddle.y + paddle_size-1):
+    elif ball.x == 2 and (left_paddle.x <= ball.y <= left_paddle.y + left_paddle.size-1):
         x_velocity = -x_velocity
 
 
