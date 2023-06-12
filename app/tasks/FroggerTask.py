8d37a6e25445ecaffc00bdfc402febba4b7ca1e1
DaveArrowsmith <dave.arrowsmith@madetech.com>
2023-06-12 14:18:10 +0100

    Refactor.

--- app/tasks/FroggerTask.py
+++ app/tasks/FroggerTask.py
@@ -1,8 +1,14 @@
 from .BaseTask import BaseTask
 
-# Entities
+class Colours:
+    def __init__(self):
+        self.red = (255, 50, 50)
+        self.green = (100, 255, 100)
+        self.blue = (0, 0, 150)
+        self.yellow = (255, 255, 20)
 
-class Entity:
+
+class VisualElement:
     def __init__(self, colour, height, width, x_start, y_start):
         self.colour = colour
         self.height = height
@@ -19,14 +25,15 @@ class Entity:
         self.x = self.x_start
         self.y = self.y_start
 
-class MovingEntity(Entity):
+
+class Frog(VisualElement):
     def __init__(self, colour, height, width, x_start, y_start, x_limit, y_limit):
         super().__init__(colour, height, width, x_start, y_start)
 
         self.x_limit = x_limit
         self.y_limit = y_limit
 
-    def update_position(self, x_change, y_change):
+    def move(self, x_change, y_change):
         new_x = self.x + x_change
         new_y = self.y + y_change
 
@@ -36,47 +43,38 @@ class MovingEntity(Entity):
         if new_y >= 0 and new_y <=self.y_limit:
             self.y = new_y
 
-class Frog(MovingEntity):
-    def __init__(self, colour, height, width, x_start, y_start, x_limit, y_limit):
-        super().__init__(colour, height, width, x_start, y_start, x_limit, y_limit)
-
-
-# Game
 
 class FroggerGame():
     def __init__(self, x_boundary, y_boundary):
         self.x_boundary = x_boundary
         self.y_boundary = y_boundary
+        self.colours = Colours()
 
-        self.colour_red = (255, 50, 50)
-        self.colour_green = (100, 255, 100)
+        self.frog = Frog(self.colours.green, 1, 1, 1, 3, self.x_boundary, self.y_boundary)
 
-        self.frog = Frog(self.colour_green, 1, 1, 1, 3, self.x_boundary, self.y_boundary)
-
-    def button_watcher(self, input):
-        if input == 'left_1':
-            self.frog.update_position(0, -1)
+    def button_watcher(self, button):
+        if button == 'left_1':
+            self.frog.move(0, -1)
             
-        if input == 'left_2':
-            self.frog.update_position(0, 1)
+        if button == 'left_2':
+            self.frog.move(0, 1)
 
-        if input == 'right_1':
-            self.frog.update_position(1, 0)
+        if button == 'right_1':
+            self.frog.move(1, 0)
 
-        if input == 'right_2':
-            self.frog.update_position(-1, 0)
+        if button == 'right_2':
+            self.frog.move(-1, 0)
 
-
-# Task
 
 class FroggerTask(BaseTask):
     def __init__(self, settings):
         super().__init__(settings)
         self.display_width = 15
         self.display_height = 6
+        self.game = FroggerGame(self.display_width, self.display_height)
 
     def start(self):
-        self.game = FroggerGame(self.display_width, self.display_height)
+        pass
 
     def stop(self):
         pass
@@ -99,4 +97,5 @@ class FroggerTask(BaseTask):
 
     def render(self, screen):
         screen.clear()
+
         screen.rectangle(*self.game.frog.get_properties())
