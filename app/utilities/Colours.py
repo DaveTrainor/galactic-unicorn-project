d7554c3c1b75b0ae85e9153e7a08e8aeebec40b1
DaveArrowsmith <dave.arrowsmith@madetech.com>
2023-06-15 14:20:59 +0100

    Frogger: Move Colours class into a separate utility file.

---
+++ app/utilities/Colours.py
@@ -0,0 +1,11 @@
+class Colours:
+    def __init__(self):
+        self.black = (0, 0, 0)
+        self.blue = (0, 0, 255)
+        self.cyan = (0, 255, 255)
+        self.green = (0, 255, 0)
+        self.green_light = (100, 255, 100)
+        self.purple = (255, 0, 255)
+        self.red = (255, 0, 0)
+        self.white = (255, 255, 255)
+        self.yellow = (255, 255, 0)
